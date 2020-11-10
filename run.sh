615d3111a7e39e9b6d5e35462a97a90144103ccf
urbaxe <urbaxe@fixdata.ws>
2020-11-10 01:29:02 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-wget -qO- http://releases.domoticz.com/releases/release/domoticz_linux_x86_64.tgz | tar xz -C /opt/domoticz
+wget -qO- https://releases.domoticz.com/releases/beta/domoticz_linux_x86_64.tgz | tar xz -C /opt/domoticz
 chmod +x ./domoticz
 sed -i '/update2.html/d' /opt/domoticz/www/html5.appcache
 cd /opt/domoticz/plugins && git clone https://github.com/d-EScape/Domoticz_iDetect.git iDetect
