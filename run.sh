2ffa0a11a5733b99ceea47ca74f3c2b11341d9ff
urbaxe <urbaxe@fixdata.ws>
2020-11-12 13:35:27 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+echo "*******************************"
+echo "**** DOMOTICZ STARTING UP *****"
+echo "*******************************"
+
+. /commons.sh
+
+# Check if we need to configure the container timezone
+if [ ! -z "$TZ" ]; then
+        TZ_FILE="/usr/share/zoneinfo/$TZ"
+        if [ -f "$TZ_FILE" ]; then
+                echo  "$notice Setting container timezone to: ${emphasis}$TZ${reset}"
+                ln -snf "$TZ_FILE" /etc/localtime
+                echo "$TZ" > /etc/timezone
+        else
+                echo "$warn Cannot set timezone to: ${emphasis}$TZ${reset} -- this timezone does not exist."
+        fi
+else
+        echo "$info Not setting any timezone for the container"
+fi
+
+rsync -a --remove-source-files /tmp/domoticz/plugins $HOME
+rsync -a --remove-source-files /tmp/domoticz/scripts $HOME
+cd $HOME
+chmod +x $HOME/domoticz
+$HOME/domoticz -dbase $HOME/db/domoticz.db -log $HOME/db/domoticz.log -www $www -sslwww $sslwww
